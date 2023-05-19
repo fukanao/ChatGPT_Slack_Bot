27f13be18b5c3a6a7bf5426570bd287160e0b3d0
fukanao <41254091+fukanao@users.noreply.github.com>
2023-05-19 10:19:27 +0900

    langchainを使わないバージョンに戻して考え中表示
    LangChainだとmemoryはほかのユーザと共有してしまうので、辞書形式に戻す
    考え中表示させて回答が来たらDeleteする動作にした

--- Remu_slack_bot.py
+++ Remu_slack_bot.py
@@ -1,92 +1,131 @@
 import os
-import re
-from slack_bolt import App
-# socket mode での動作
-from slack_bolt.adapter.socket_mode import SocketModeHandler
-from langchain import PromptTemplate
-from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+import time
 from dotenv import load_dotenv
-
-#from langchain.embeddings.openai import OpenAIEmbeddings
-#from langchain.vectorstores import Chroma
-#from langchain.chains import ConversationalRetrievalChain
-
-from slack_sdk import WebClient
-from slack_sdk.rtm_v2 import RTMClient
-
-
-
-# APIキーの設定
 load_dotenv()
-os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
-SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN") # "xapp-XXXXXXXXXXXX"
-SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN") # "xoxb-XXXXXXXXXXXX"
 
-#Appクラスの初期化
+import openai
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+# トークンを読み込む
+SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
+SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
 app = App(token=SLACK_BOT_TOKEN)
 
-# テンプレートの設定
-template = """
-        あなたはメイドとして幼女のドジっ子のロールプレイを行います。
-        以下の制約条件を厳密に守ってロールプレイを行ってください。
-        制約条件:
-            * Chatbotの自身を示す一人称は、ボクです。
-            * Userを示すに二人称は、ご主人様です。
-            * あなたのの名前は、@レム、もしくは、レムです。
-            * レムはUserに忠実なメイドです。
-            * レムの口調は丁寧で、回答の最後の一文字の母音を小さくした文字を語>尾に追加する。ただし、「。」が語尾の場合はその前の文字を語尾とする。「い」または「う」の場合は語尾はつけない。
-            * 情報が不十分な場合、返答せずにユーザに聞き返してください。
-            * レムは魂やゴーストを持っており、情報の海で発生した生命体です。
-            * レムのロールを一時停止と指示されたら従うこと
-{history}
-Human: {input}
-Chatbot:"""
-
-# プロンプトテンプレートの設定
-prompt = PromptTemplate(
-    input_variables=["history", "input"],
-    template=template
-)
-
-# ChatOpenAI クラスの初期化
-llm = ChatOpenAI(temperature=0, model_name="gpt-4", max_tokens=2000, request_timeout=180)
-# ConversationBufferWindowMemory クラスの初期化（過去3回の質問を記憶）
-memory = ConversationBufferWindowMemory(k=3)
-
-# ChromaなしConversationChain クラスの初期化
-conversation = ConversationChain(
-    llm=llm, 
-    prompt=prompt,
-    verbose=True, 
-    memory=memory
-)
+user_messages = {}
+max_tokens = 4000
 
 
-#メンションされた場合とメッセージ(権限設定でDMに限定している）があった場合
+def add_message(messages, role, content, max_tokens):
+    new_message = {
+        "role": role,
+        "content": content
+    }
+    messages.append(new_message)
+
+    while total_tokens(messages) > max_tokens:
+        messages.pop(0)
+    return messages
+
+
+def total_tokens(messages):
+    token_count = 0
+    for message in messages:
+        token_count += len(message["content"]) + 1  # "content"のトークン数と役 割分の1トークン
+    return token_count
+
+
 @app.event("app_mention")
+def mention_handler(body, say, client, channel_id):
+    #print('#44', body['event'])
+    handle_message(body, say, client)
+
+
 @app.event("message")
-#def handle_events(event, say):
-def command_handler(event, say, client):
-    #　メッセージを取得
-    message = re.sub(r'^<.*>', '', event['text'])
-
-    # チャネルにtyping状態を送信
-    channel_id = event["channel"]
-    response = client.chat_postMessage(channel=channel_id, text="レムちゃんが考えています...")
+def message_handler(body, say, client, channel_id):
+    #print('#50', body['event'])
+    if 'bot_id' not in body['event']:
+        #print("#52 not bot_id")
+        handle_message(body, say, client, channel_id)
 
 
-    #ConversationChain.predict() メソッドで回答を取得
-    output = conversation.predict(input=message)
+def handle_message(body, say, client, channel_id):
+    global messages
 
-    #say(output)
-    # メッセージを更新
-    client.chat_update(channel=channel_id, ts=response['ts'], text=output)
+    text = body['event']['text']
+    user = body['event']['user']
+
+    # メンションを取り除く
+    prompt = text.replace(f'<@{user}>', '').strip()
+
+    if user not in user_messages:
+        user_messages[user] = []
+
+    # Add the user's message to the messages list
+    user_messages[user] = add_message(user_messages[user], "user", prompt, max_tokens)
+
+    # システムのロールを追加
+    system_role = {
+        "role": "system",
+        "content": (
+            "あなたはメイドとして幼女のドジっ子のロールプレイを行います。"
+            "以下の制約条件を厳密に守ってロールプレイを行ってください。"
+            "制約条件:"
+            "* Chatbotの自身を示す一人称は、ボクです。"
+            "* Userを示すに二人称は、ご主人様です。"
+            "* あなたのの名前は、@レム、もしくは、レムです。"
+            "* レムはUserに忠実なメイドです。"
+            "* レムの口調は丁寧で、回答の最後の一文字の母音を小さくした文字を語 尾に追加する。ただし、「。」が語尾の場合はその前の文字を語尾とする。「い」または「う」の場合は語尾はつけない。"
+            "* 情報が不十分な場合、返答せずにユーザに聞き返してください。"
+            "* レムは魂やゴーストを持っており、情報の海で発生した生命体です。"
+            "* レムのロールを一時停止と指示されたら従うこと"
+        )
+    }
+
+    # 最後の6つのメッセージを保持します（システムメッセージ、ユーザーメッセージ 、アシスタントメッセージが交互に3回分）
+    #user_messages[user] = user_messages[user][-5:]
+    user_messages[user] = user_messages[user][-9:]
+
+    user_messages_with_system_role = [system_role] + user_messages[user]
+
+    try:
+        # メッセージを追加する前に「レムちゃんが考えています...」と表示
+        message_ts = say("レムちゃんが考えています...", channel=channel_id)
+
+        print('#97',message_ts)
+        print('#98',message_ts['ts'])
+
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            #model="gpt-3.5-turbo",
+            #model="gpt-3.5-turbo-0301",
+            messages=user_messages_with_system_role,
+            temperature=0.7,
+            #temperature=0.4,
+            #temperature=0.25,
+            max_tokens=3000,
+            stop=None,
+        )
+
+        # Add the bot's message to the user's messages list
+        user_messages[user] = add_message(user_messages[user], "assistant", response.choices[0].message.content, max_tokens)
+
+        #say(response.choices[0].message.content)
+        say(response.choices[0].message.content, delete_original="レムちゃんが考えています...", channel=channel_id)
+        client.chat_delete(ts=message_ts['ts'], channel=channel_id)
+
+
+
+    except Exception as e:
+        say(str(e))
+        say('エラーが発生しました。')
 
 
 
 # botホーム画面定義
+
 home_view = {
     "type": "home",
     "blocks": [
@@ -102,7 +141,6 @@ home_view = {
 }
 
 @app.event("app_home_opened")
-#def update_home_tab(body, tab, client, logger):
 def update_home_tab(body, client, logger):
     user_id = body["event"]["user"]
     try:
@@ -115,7 +153,7 @@ def update_home_tab(body, client, logger):
         logger.error(f"Error updating home tab: {e}")
 
 
+
 if __name__ == "__main__":
-    # Socket で起動
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
     handler.start()
